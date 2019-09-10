75d374b116062b63501b4ee860ded6ce4f5857fd
wangwei <jike_wangwei@163.com>
2019-09-10 20:09:23 +0800

    天气图顺序

--- src/pages/create-note/index.js
+++ src/pages/create-note/index.js
@@ -2,20 +2,50 @@ const app = getApp()
 
 Page({
     data: {
-        nodes: [{
-            name: 'div',
-            attrs: {
-                class: 'div_class',
-                style: 'line-height: 60px; color: red;'
+        memo: '',
+        indicatorDots: false,
+        autoplay: false,
+        interval: 5000,
+        duration: 1000,
+        weatherList: [
+            {
+                weather: 'sunny',
+                word: '风和日丽'
             },
-            children: [{
-                type: 'text',
-                text: 'Hello&nbsp;World!'
-            }]
-        }]
+            {
+                weather: 'cloudy',
+                word: '如晴天似雨天'
+            },
+            {
+                weather: 'stormy',
+                word: '电闪雷鸣'
+            },
+            {
+                weather: 'rainy',
+                word: '风雨交加'
+            },
+            {
+                weather: 'snowy',
+                word: '千里冰封'
+            },
+            {
+                weather: 'rainbow',
+                word: '雨过天晴'
+            },
+            {
+                weather: 'starry',
+                word: '繁星点点'
+            }
+        ]
     },
     tap() {
         console.log('tap')
+    },
+    swiperChange: function (e) {
+        console.log(e);
+        this.setData({
+            swiperCurrent: e.detail.current   //获取当前轮播图片的下标
+        })
     }
 })
 
